b38996cf56d43ca98ff2df53b2ca4eea33f6ad43
Tupay Machado <tupay.machado@gmail.com>
2023-06-01 17:19:41 -0300

    trabalho em relação a auth

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCCVI3ns8bDvMKgHX_H5u6y4TeF7uf4K84",
@@ -11,4 +13,42 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const btnLogin = document.getElementById("btnLogin");
 
+btnLogin.addEventListener("click", login);
+emailInput.addEventListener("change", validateFields);
+passwordInput.addEventListener("change", validateFields);
+
+function validateFields() {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    const isEmailValid = validateEmail(email);
+    const isPasswordValid = validatePassword(password);
+
+    btnLogin.disabled = !(isEmailValid && isPasswordValid);
+}
+
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function validatePassword(password) {
+    return !!password;
+}
+//create a login function with firebase auth that redirects user to status.html if login is successful
+function login() {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+        .then(response => {
+            window.location.href = "status.html";
+        })
+        .catch((error) => {
+            alert("Erro ao realizar login");
+        });
+}
