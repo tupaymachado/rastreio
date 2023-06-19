4840ab9d0166ed987e21a34bdb163a9acf1dc171
Tupay Machado <tupay.machado@gmail.com>
2023-06-19 16:04:57 -0300

    Refatoração da tabela de status e função

--- public/main.js
+++ public/main.js
@@ -15,9 +15,8 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const uid = localStorage.getItem('uid');
 
-document.getElementById('enviosQt').addEventListener('change', function () {
-    const quantidade = parseInt(this.value);
-    loadStatus(quantidade);
+document.getElementById('statusQt').addEventListener('change', function () {
+    loadStatus();
 });
 
 function loadEnvios() {
@@ -37,7 +36,6 @@ loadEnvios()
 
 function loadUser() {
     return new Promise((resolve, reject) => {
-        console.log(uid)
         const userRef = ref(database, `users/${uid}`);
         onValue(userRef, (snapshot) => {
             const data = snapshot.val();
@@ -54,52 +52,46 @@ function loadUser() {
 };
 loadUser()
 
+function loadEnvios() {
+
+    const tabelaEnvios = document.getElementById("envios-table-body");
+    tabelaEnvios.innerHTML = '';
+}
+
 function loadStatus() {
-    console.log('loadStatus')
-    const tabelaOrigem = document.getElementById("tabela-origem").getElementsByTagName("tbody")[0];
-    const tabelaCD = document.getElementById("tabela-cd").getElementsByTagName("tbody")[0];
-    const tabelaRecebido = document.getElementById("tabela-recebido").getElementsByTagName("tbody")[0];
-    tabelaCD.innerHTML = '';
-    tabelaOrigem.innerHTML = '';
-    tabelaRecebido.innerHTML = '';
-    const enviosQt = document.getElementById("enviosQt").value;
+    const tabelaStatus = document.getElementById("status-table-body");
+    tabelaStatus.innerHTML = '';
     const enviosMain = [];
     for (let i = 0; i < window.envios.length; i++) {
         if (window.envios[i].origem.Destino.toLowerCase() == user.toLowerCase()) { //jogar os envios selecionados para um outro array e fazer ele aparecer dali
             enviosMain.push(window.envios[i])
         }
-    }   
-    for (let j = 0; j < enviosMain.length; j++) {
-        console.log(enviosMain[j])
-        const novaLinhaOrigem = document.createElement("tr");
-        novaLinhaOrigem.innerHTML = `
+    }
+    const statusQt = (document.getElementById("statusQt").value > enviosMain.length) ? enviosMain.length : document.getElementById("statusQt").value;
+    for (let j = enviosMain.length - statusQt; j < enviosMain.length; j++) {
+        let envioStatus = '';
+        if (!enviosMain[j].destino['Chegada'] && !enviosMain[j].cd['Responsavel pela saida']) {
+            envioStatus = `<td>Em trânsito</td>`;
+        } else if (!enviosMain[j].destino['Chegada']) {
+            envioStatus = `<td><input type='checkbox'>Recebido?</input></td>`; //inserir função que muda o status para recebido
+        } else {
+            envioStatus = `<td>Recebido</td>`;
+        }
+        const novaLinha = document.createElement("tr");
+        novaLinha.innerHTML = `
                 <td>${enviosMain[j].origem['Codigo do Malote']}</td>
                 <td>${enviosMain[j].origem['Origem']}</td>
                 <td>${enviosMain[j].origem['Responsavel']}</td>
                 <td>${enviosMain[j].origem['Saida']}</td>
                 <td>${enviosMain[j].origem['Destino']}</td>
                 <td>${enviosMain[j].origem['Transportador']}</td>
-            `;
-        tabelaOrigem.appendChild(novaLinhaOrigem); //adiciona a linha na tabela de origem
-        const novaLinhaCD = document.createElement("tr");
-        novaLinhaCD.innerHTML = `
                 <td>${enviosMain[j].cd['Chegada']}</td>
                 <td>${enviosMain[j].cd['Responsavel pelo recebimento']}</td>
                 <td>${enviosMain[j].cd['Saida']}</td>
                 <td>${enviosMain[j].cd['Responsavel pela saida']}</td>
                 <td>${enviosMain[j].cd['Transportador']}</td>
+                ${envioStatus}
             `;
-        tabelaCD.appendChild(novaLinhaCD);
-        const novaLinhaRecebido = document.createElement("tr");
-        if (!enviosMain[j].destino['Chegada'] && !enviosMain[j].cd['Responsavel pela saida']) {
-            novaLinhaRecebido.innerHTML = `<td>Em trânsito</td>`;
-            tabelaRecebido.appendChild(novaLinhaRecebido);
-        } else if (!enviosMain[j].destino['Chegada']) {
-            novaLinhaRecebido.innerHTML = `<td><input type='checkbox'>Recebido?</input></td>`; //inserir função que muda o status para recebido
-            tabelaRecebido.appendChild(novaLinhaRecebido);
-        } else {
-            novaLinhaRecebido.innerHTML = `<td>Recebido</td>`;
-            tabelaRecebido.appendChild(novaLinhaRecebido);
-        }
+        tabelaStatus.appendChild(novaLinha);
     }
 }
